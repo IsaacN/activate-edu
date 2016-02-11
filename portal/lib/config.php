0d174c1e7391abe90c01d826f6b7d5a75a640095
IsaacN <me@isaacnankavill.com>
2016-02-11 14:37:29 +1000

    Payments Initial

---
+++ portal/lib/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array(
+
+	/*
+	* ADMIN ACCESS CREDENTIALS
+	*/
+	'admin_username' => 'admin', // username used to login to the admin area
+	'admin_password' => 'adminpassword', // password used to login to the admin area
+
+	/*
+	* DATABASE ACCESS CREDENTIALS
+	*/
+	'db_host' => 'localhost', // database host, usually localhost
+	'db_username' => 'frictio3_actv', // database username
+	'db_password' => 'CaptainMater14l!', // datebase password
+	'db_name' => 'fricto3_activate-edu', // database name
+
+);
